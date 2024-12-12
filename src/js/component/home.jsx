a30ae5d15dc68c99a288482230807654f1cc88bd
Ruben <167398241+rumacon@users.noreply.github.com>
2024-12-12 03:57:29 +0000

    SA

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
 import React from "react";
+import {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	
+	let [puchado, prendido] = useState();
+
+  const clickeado = (color) => {
+    prendido(color);
+  };
+
+  return (
+    <div className="position-absolute top-0 end-50">
+	<div className="poste"></div>
+      <div className="semaforo">
+        <div
+          className={
+            "bola redLight " + (puchado == "red" ? "prende" : "lightRed")
+          }
+          onClick={() => clickeado("red")}
+        ></div>
+        <div
+          className={
+            "bola yellowLight " + (puchado == "yellow" ? "prende" : "lightYellow")
+          }
+          onClick={() => clickeado("yellow")}
+        ></div>
+        <div
+          className={
+            "bola greenLight " + (puchado == "green" ? "prende" : "lightGreen")
+          }
+          onClick={() => clickeado("green")}
+        ></div>
+
+      </div>
+	  </div>
+  );
+}
+
 
 export default Home;
+
